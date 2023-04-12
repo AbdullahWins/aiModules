091433698d38f379f9730224f453bd125cb4671e
Abdullah Al MahMud <abdudevs@gmail.com>
2023-04-12 16:41:13 +0600

    fixed ai assistand page

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import TopNav from "../components/Shared/TopNav";
 import Footer from "../components/Shared/Footer";
-import "./Main.css";
 
 const Main = () => {
   return (
-    <section className="min-h-screen flex flex-col justify-center md:justify-between bg-whiteMid mainBackground">
+    <section className="min-h-screen flex flex-col justify-center md:justify-between bg-whiteMid ">
       <TopNav></TopNav>
       <Outlet></Outlet>
       <Footer></Footer>
