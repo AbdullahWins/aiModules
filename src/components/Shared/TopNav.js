cee77ed03c2e968b85aa2683d33350d7c4f59c8b
Abdullah Al MahMud <abdudevs@gmail.com>
2023-05-02 16:17:44 +0600

    added dynamic page title and menu style

--- src/components/Shared/TopNav.js
+++ src/components/Shared/TopNav.js
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo/logo.png";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/AuthContext";
+import { useState } from "react";
 
 const TopNav = () => {
   const { user, logout } = useContext(AuthContext);
+  const [activePage, setActivePage] = useState(document.title);
+
+  console.log(activePage);
 
   return (
     <section className="max-w-6xl w-full mx-auto my-0 px-4 md:py-9">
@@ -15,17 +19,52 @@ const TopNav = () => {
         </div>
         <div className="flex gap-8 py-2">
           <Link to="/">
-            <button className="text-btnNavColor hover:text-btnHoverColor">
+            <button
+              onClick={() => {
+                setActivePage(
+                  `${process.env.REACT_APP_ApplicationName} | Home`
+                );
+              }}
+              className={`btn btn-ghost btn-sm rounded-full border-none hover:text-btnColor normal-case ${
+                activePage === `${process.env.REACT_APP_ApplicationName} | Home`
+                  ? "text-whiteMid bg-btnColor"
+                  : "text-btnNavColor"
+              }`}
+            >
               Home
             </button>
           </Link>
           <Link to="/pricing">
-            <button className="text-btnNavColor hover:text-btnHoverColor">
+            <button
+              onClick={() => {
+                setActivePage(
+                  `${process.env.REACT_APP_ApplicationName} | Pricing`
+                );
+              }}
+              className={`btn btn-ghost btn-sm rounded-full border-none hover:text-btnColor normal-case ${
+                activePage ===
+                `${process.env.REACT_APP_ApplicationName} | Pricing`
+                  ? "text-whiteMid bg-btnColor"
+                  : "text-btnNavColor"
+              }`}
+            >
               Pricing
             </button>
           </Link>
           <Link to="/about">
-            <button className="text-btnNavColor hover:text-btnHoverColor">
+            <button
+              onClick={() => {
+                setActivePage(
+                  `${process.env.REACT_APP_ApplicationName} | About`
+                );
+              }}
+              className={`btn btn-ghost btn-sm rounded-full border-none hover:text-btnColor normal-case ${
+                activePage ===
+                `${process.env.REACT_APP_ApplicationName} | About`
+                  ? "text-whiteMid bg-btnColor"
+                  : "text-btnNavColor"
+              }`}
+            >
               About Myself
             </button>
           </Link>
@@ -34,12 +73,25 @@ const TopNav = () => {
             rel="noopener noreferrer"
             to="https://netrocreative.com/"
           >
-            <button className="text-btnNavColor hover:text-btnHoverColor">
+            <button
+              onClick={() => {
+                setActivePage(
+                  `${process.env.REACT_APP_ApplicationName} | Our Website`
+                );
+              }}
+              className={`btn btn-ghost btn-sm rounded-full border-none hover:text-btnColor normal-case ${
+                activePage ===
+                `${process.env.REACT_APP_ApplicationName} | Our Website`
+                  ? "text-whiteMid bg-btnColor"
+                  : "text-btnNavColor"
+              }`}
+            >
               Our Website
             </button>
           </Link>
           {user ? (
             <button
+              className={`btn btn-ghost btn-sm rounded-full border-none hover:text-errorColor hover:bg-whiteLow normal-case`}
               onClick={() => {
                 logout();
               }}
@@ -48,7 +100,19 @@ const TopNav = () => {
             </button>
           ) : (
             <Link to="/login">
-              <button className="rounded-full text-btnNavColor hover:text-btnHoverColor">
+              <button
+                onClick={() => {
+                  setActivePage(
+                    `${process.env.REACT_APP_ApplicationName} | Login`
+                  );
+                }}
+                className={`btn btn-ghost btn-sm rounded-full border-none hover:text-btnColor normal-case ${
+                  activePage ===
+                  `${process.env.REACT_APP_ApplicationName} | Login`
+                    ? "text-whiteMid bg-btnColor"
+                    : "text-btnNavColor"
+                }`}
+              >
                 Login
               </button>
             </Link>
